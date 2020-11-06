89b484a2b77f5c3251324aa5e3bfb2309168dd3d
DESKTOP-7R39I3C\emilo <olejnike2000@gmail.com>
2020-11-06 18:03:23 +0100

    Added simple move of the parts
    Moreover, imported TextMeshPro, added some materials and a lot of meta files

---
+++ Assets/CoordsDisplay.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+[ExecuteInEditMode]
+public class CoordsDisplay : MonoBehaviour
+{
+    //Display Waypoint Coords
+    //REMOVE THIS SCRIPT FROM GAMEOBJECT WHEN GAME IS FINISHED
+    [SerializeField] TextMeshPro coordsLabel;
+    private void Update()
+    {
+        float xPos = transform.position.x;
+        float zPos = transform.position.z;
+        xPos = Mathf.RoundToInt(xPos);
+        zPos = Mathf.RoundToInt(zPos);
+        coordsLabel.text = xPos.ToString() + " , " + zPos.ToString();
+
+    }
+}
---
+++ Assets/Scripts/PartsMovement.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartsMovement : MonoBehaviour
+{
+    
+    Queue<Waypoint> queueofWaypoints = new Queue<Waypoint>();
+    List<Waypoint> testlist = new List<Waypoint>();
+    [SerializeField] WaypointManager waypointManager;
+    Waypoint[] arrayedQueue;
+    Vector3 direction;
+    float distance;
+    void Start()
+    {
+        var waypoints = waypointManager.waypoints;
+        EnqueueWaypoints(waypoints);
+    }
+
+    private float CalculateDistance()
+    {
+        arrayedQueue = queueofWaypoints.ToArray();
+        distance = Vector3.Distance(transform.position, arrayedQueue[1].GetWaypointPosition());
+        return distance;
+    }
+
+    private void EnqueueWaypoints(List<Waypoint> waypoints)
+    {
+        foreach(Waypoint waypoint in waypoints) 
+        {
+            queueofWaypoints.Enqueue(waypoint);
+        }
+        
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float distance = CalculateDistance();
+        direction = (transform.position - arrayedQueue[1].GetWaypointPosition());
+        //print(distance);
+        print(direction);
+        if(distance <= 0.01f) 
+        {
+            queueofWaypoints.Dequeue();
+            
+
+        }
+        else 
+        {
+            transform.position -= direction * Time.deltaTime;
+        }
+    }
+    //
+}
